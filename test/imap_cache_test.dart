c8d708ae90cfbaf68fd09af6079b64ddd2b32c0c
wuchuheng <root@wuchuheng.com>
2022-08-23 10:43:45 +0800

    feat: created env configuration.

--- test/imap_cache_test.dart
+++ test/imap_cache_test.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:test/test.dart';
+import 'package:wuchuheng_env/wuchuheng_env.dart';
 
 void main() {
   group('A group of tests', () {
@@ -9,6 +12,9 @@ void main() {
     });
 
     test('First Test', () {
+      final file = '${Directory.current.path}/test/.env';
+      final env = Load(file: file).env;
+      print(env['USER_NAME']);
       // expect(awesome.isAwesome, isTrue);
     });
   });
