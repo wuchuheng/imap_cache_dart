c366671d6bd302ddc47d6684c3b131d0f673afb1
wuchuheng <root@wuchuheng.com>
2022-08-23 17:52:27 +0800

    test: testing the connectToServer method.

--- example/imap_cache_example.dart
+++ example/imap_cache_example.dart
@@ -1,40 +1,38 @@
-import 'package:imap_cache/imap_cache.dart';
-
 void main() async {
-  final ImapCache cacheServiceInstance = await ImapCache().connectToServer(
-    userName: 'my email account',
-    password: 'password',
-    imapServerHost: 'imap host',
-    imapServerPort: 993,
-    isImapServerSecure: true,
-    boxName: 'snotes',
-  );
-
-  /// set Data
-  await cacheServiceInstance.set(key: 'foo', value: 'hello');
-
-  /// print: hello
-  print(await cacheServiceInstance.get(key: 'foo'));
-
-  /// print: true
-  print(await cacheServiceInstance.has(key: 'foo'));
-  await cacheServiceInstance.unset(key: 'foo');
-
-  /// print: false
-  print(await cacheServiceInstance.has(key: 'foo'));
-
-  ///  Trigger setting events after set the foo variables
-  await cacheServiceInstance.set(key: 'foo', value: 'hello');
-  final unsetSubscribeHandle = cacheServiceInstance.beforeSet(
-      key: 'foo',
-      callback: ({required String key, required String value, required String hash}) async {
-        return 'new value';
-      });
-
-  ///  Trigger unseting events after delete the foo variables
-  await cacheServiceInstance.unset(key: 'foo');
-
-  /// Unsubscribe from events for foo key
-  unsetSubscribeHandle.unsubscribe();
-  unsetSubscribeHandle.unsubscribe();
+  // final ImapCacheService cacheServiceInstance = await ImapCacheService().connectToServer(
+  //   userName: 'my email account',
+  //   password: 'password',
+  //   imapServerHost: 'imap host',
+  //   imapServerPort: 993,
+  //   isImapServerSecure: true,
+  //   boxName: 'snotes',
+  // );
+  //
+  // /// set Data
+  // await cacheServiceInstance.set(key: 'foo', value: 'hello');
+  //
+  // /// print: hello
+  // print(await cacheServiceInstance.get(key: 'foo'));
+  //
+  // /// print: true
+  // print(await cacheServiceInstance.has(key: 'foo'));
+  // await cacheServiceInstance.unset(key: 'foo');
+  //
+  // /// print: false
+  // print(await cacheServiceInstance.has(key: 'foo'));
+  //
+  // ///  Trigger setting events after set the foo variables
+  // await cacheServiceInstance.set(key: 'foo', value: 'hello');
+  // final unsetSubscribeHandle = cacheServiceInstance.beforeSet(
+  //     key: 'foo',
+  //     callback: ({required String key, required String value, required String hash}) async {
+  //       return 'new value';
+  //     });
+  //
+  // ///  Trigger unseting events after delete the foo variables
+  // await cacheServiceInstance.unset(key: 'foo');
+  //
+  // /// Unsubscribe from events for foo key
+  // unsetSubscribeHandle.unsubscribe();
+  // unsetSubscribeHandle.unsubscribe();
 }
--- test/imap_cache_test.dart
+++ test/imap_cache_test.dart
@@ -1,5 +1,8 @@
 import 'dart:io';
 
+import 'package:imap_cache/imap_cache.dart';
+import 'package:imap_cache/src/dto/connect_config/index.dart';
+import 'package:imap_cache/src/service/imap_cache_service/index_abstarct.dart';
 import 'package:test/test.dart';
 import 'package:wuchuheng_env/wuchuheng_env.dart';
 
@@ -11,11 +14,19 @@ void main() {
       // Additional setup goes here.
     });
 
-    test('First Test', () {
-      final file = '${Directory.current.path}/test/.env';
-      final env = Load(file: file).env;
-      print(env['USER_NAME']);
-      // expect(awesome.isAwesome, isTrue);
+    final file = '${Directory.current.path}/test/.env';
+    final env = Load(file: file).env;
+    test('Connect Test', () async {
+      final config = ConnectConfig(
+        userName: env['USER_NAME']!,
+        password: env['PASSWORD']!,
+        imapServerHost: env['HOST']!,
+        imapServerPort: int.parse(env['PORT']!),
+        isImapServerSecure: env['TLS']!.toLowerCase() == 'true',
+        boxName: env['BOX_NAME']!,
+        localCacheDirectory: env['LOCAL_CACHE_DIRECTORY']!,
+      );
+      ImapCacheServiceAbstract imapCache = await ImapCache().connectToServer(config);
     });
   });
 }
