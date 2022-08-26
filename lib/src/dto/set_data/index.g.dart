4e1ad458dd030b7729605f4b62e2fb8f18504bcb
wuchuheng <root@wuchuheng.com>
2022-08-26 18:58:33 +0800

    feat: simplify the code.

---
+++ lib/src/dto/set_data/index.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'index.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+SetData _$SetDataFromJson(Map<String, dynamic> json) => SetData(
+      key: json['key'] as String,
+      value: json['value'] as String,
+    );
+
+Map<String, dynamic> _$SetDataToJson(SetData instance) => <String, dynamic>{
+      'key': instance.key,
+      'value': instance.value,
+    };
